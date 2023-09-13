96096dceda43cc04ceaa516fe369e5847de4369c
Andrea Severi <andrea.severi.dev@gmail.com>
2023-09-13 21:14:52 +0200

    add use cases to view model

--- app/src/main/kotlin/com/mitch/dimlight/ui/screens/home/HomeViewModel.kt
+++ app/src/main/kotlin/com/mitch/dimlight/ui/screens/home/HomeViewModel.kt
@@ -1,59 +1,21 @@
 package com.mitch.dimlight.ui.screens.home
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.mitch.dimlight.domain.repository.UserSettingsRepository
-import com.mitch.dimlight.ui.util.Result
-import com.mitch.dimlight.ui.util.asResult
-import com.mitch.dimlight.util.DimlightLanguage
-import com.mitch.dimlight.util.DimlightTheme
+import com.mitch.dimlight.domain.model.FlashlightDimLevel
+import com.mitch.dimlight.domain.usecase.flashlight.FlashlightUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val userSettingsRepository: UserSettingsRepository
+    private val flashlightUseCases: FlashlightUseCases
 ) : ViewModel() {
 
-    val uiState = combine(
-        userSettingsRepository.getLanguage(),
-        userSettingsRepository.getTheme(),
-        ::Pair
-    ).asResult()
-        .map { result ->
-            when (result) {
-                Result.Loading -> HomeUiState.Loading
-                is Result.Success -> {
-                    val (language, theme) = result.data
-
-                    HomeUiState.Success(
-                        language = language,
-                        theme = theme
-                    )
-                }
-
-                is Result.Error -> HomeUiState.Error()
-            }
-        }.stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(5_000),
-            initialValue = HomeUiState.Loading
-        )
-
-    fun updateTheme(theme: DimlightTheme) {
-        viewModelScope.launch {
-            userSettingsRepository.setTheme(theme)
-        }
+    fun turnOnFlashlight(level: FlashlightDimLevel) {
+        flashlightUseCases.turnOnFlashlight(level)
     }
 
-    fun updateLanguage(language: DimlightLanguage) {
-        viewModelScope.launch {
-            userSettingsRepository.setLanguage(language)
-        }
+    fun turnOffFlashlight() {
+        flashlightUseCases.turnOffFlashlight()
     }
 }
