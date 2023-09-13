add593a0ed954b64305083b0c5fb823fe73bb589
Andrea Severi <andrea.severi.dev@gmail.com>
2023-09-13 21:14:28 +0200

    create function to convert number from one range to another

---
+++ app/src/main/kotlin/com/mitch/dimlight/util/extension/Range.kt
@@ -0,0 +1,6 @@
+package com.mitch.dimlight.util.extension
+
+fun Int.convertFromRangeToAnother(original: IntRange, target: IntRange): Int {
+    val ratio = this.toFloat() / (original.last - original.first)
+    return (ratio * (target.last - target.first)).toInt()
+}
