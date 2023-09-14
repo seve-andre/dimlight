63968b0d9b0bd4ba9c44cb718c02c9864d46485c
Andrea Severi <andrea.severi.dev@gmail.com>
2023-09-14 15:11:46 +0200

    remove unused function

--- app/src/main/kotlin/com/mitch/dimlight/domain/model/FlashlightDimFixedLevel.kt
+++ app/src/main/kotlin/com/mitch/dimlight/domain/model/FlashlightDimFixedLevel.kt
@@ -1,16 +1,7 @@
 package com.mitch.dimlight.domain.model
 
-import com.mitch.dimlight.util.extension.convertFromRangeToAnother
-
 sealed class FlashlightDimFixedLevel(val value: Int) {
     data object Min : FlashlightDimFixedLevel(1)
     data object Half : FlashlightDimFixedLevel(50)
     data object Max : FlashlightDimFixedLevel(100)
-
-    fun convertToFlashlightRange(flashlightRange: IntRange): Int {
-        val min = Min.value
-        val max = Max.value
-
-        return this.value.convertFromRangeToAnother(min..max, flashlightRange)
-    }
 }
