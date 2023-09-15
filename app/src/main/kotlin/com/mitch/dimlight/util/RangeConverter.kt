76c678629338cba15de3c9808608a17ff89eabff
Andrea Severi <andrea.severi.dev@gmail.com>
2023-09-15 16:01:35 +0200

    create range converter class and test

--- app/src/main/kotlin/com/mitch/dimlight/util/RangeConverter.kt
+++ app/src/main/kotlin/com/mitch/dimlight/util/RangeConverter.kt
@@ -1,1 +1,42 @@
 package com.mitch.dimlight.util
+
+class RangeConverter<T> where T : Number, T : Comparable<T> {
+    private lateinit var numberToConvert: T
+    private lateinit var fromRange: ClosedRange<T>
+    private lateinit var toRange: ClosedRange<T>
+
+    fun convert(number: T): RangeConverter<T> {
+        this.numberToConvert = number
+        return this
+    }
+
+    fun fromRange(range: ClosedRange<T>): RangeConverter<T> {
+        check(range.contains(numberToConvert))
+
+        this.fromRange = range
+        return this
+    }
+
+    fun toRange(range: ClosedRange<T>): T {
+        this.toRange = range
+        return convertFromRangeToRange()
+    }
+
+    private fun convertFromRangeToRange(): T {
+        val fromRangeStart = fromRange.start.toDouble()
+        val fromRangeEnd = fromRange.endInclusive.toDouble()
+
+        val toRangeStart = toRange.start.toDouble()
+        val toRangeEnd = toRange.endInclusive.toDouble()
+
+        val ratio = (numberToConvert.toDouble() - fromRangeStart) / (fromRangeEnd - fromRangeStart)
+        val result = toRangeStart + (ratio * (toRangeEnd - toRangeStart))
+
+        @Suppress("UNCHECKED_CAST")
+        return result as T
+    }
+}
+
+fun <T> convert(number: T): RangeConverter<T> where T : Number, T : Comparable<T> {
+    return RangeConverter<T>().convert(number)
+}
--- app/src/test/kotlin/com/mitch/dimlight/util/RangeConverterTest.kt
+++ app/src/test/kotlin/com/mitch/dimlight/util/RangeConverterTest.kt
@@ -1,1 +1,50 @@
 package com.mitch.dimlight.util
+
+import assertk.assertFailure
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isInstanceOf
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+internal class RangeConverterTest {
+
+    @Test
+    fun `when number is not in initial range, should throw Exception`() {
+        assertFailure {
+            convert(0).fromRange(1..10)
+        }
+    }
+
+    @Test
+    fun `when number is 0, converted number should be 0`() {
+        val result = convert(0).fromRange(0..100).toRange(0..10)
+
+        assertThat(result).isEqualTo(0)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "10, 100",
+        "1, 1"
+    )
+    fun `test result is correct`(number: Int, expected: Int) {
+        val result = convert(number).fromRange(1..10).toRange(1..100)
+
+        assertThat(result).isEqualTo(expected)
+    }
+
+    @Test
+    fun `test result has correct type`() {
+        val result1 = convert(0).fromRange(0..100).toRange(0..10)
+        val result2 = convert(0f).fromRange(0f..100f).toRange(0f..10f)
+        val result3 = convert(0.0).fromRange(0.0..100.0).toRange(0.0..10.0)
+        val result4 = convert(0L).fromRange(0L..100L).toRange(0L..10L)
+
+        assertThat(result1).isInstanceOf(Int::class)
+        assertThat(result2).isInstanceOf(Float::class)
+        assertThat(result3).isInstanceOf(Double::class)
+        assertThat(result4).isInstanceOf(Long::class)
+    }
+}
