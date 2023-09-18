122a27eb46d3800a9f8354081d90083a0646a2c0
Andrea Severi <andrea.severi.dev@gmail.com>
2023-09-18 23:20:45 +0200

    rename to `BrightnessFixedLevel`

--- app/src/main/kotlin/com/mitch/dimlight/domain/model/FlashlightDimFixedLevel.kt
+++
@@ -1,7 +0,0 @@
-package com.mitch.dimlight.domain.model
-
-sealed class FlashlightDimFixedLevel(val value: Int) {
-    data object Min : FlashlightDimFixedLevel(1)
-    data object Half : FlashlightDimFixedLevel(50)
-    data object Max : FlashlightDimFixedLevel(100)
-}
