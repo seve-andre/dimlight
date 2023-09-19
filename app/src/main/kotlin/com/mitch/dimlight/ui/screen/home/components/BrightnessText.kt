a63a591dddba053608ddb0e8d0cc334ce848de27
Andrea Severi <andrea.severi.dev@gmail.com>
2023-09-19 22:27:04 +0200

    extract repeated text style

--- app/src/main/kotlin/com/mitch/dimlight/ui/screen/home/components/BrightnessText.kt
+++ app/src/main/kotlin/com/mitch/dimlight/ui/screen/home/components/BrightnessText.kt
@@ -3,10 +3,12 @@ package com.mitch.dimlight.ui.screen.home.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.mitch.dimlight.domain.model.BrightnessFixedLevel
@@ -21,20 +23,15 @@ fun BrightnessTextFraction(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(2.dp)
     ) {
-        Text(
-            text = "$brightnessLevel",
+        val textStyle = TextStyle(
             fontSize = MaterialTheme.typography.titleLarge.fontSize,
             fontWeight = FontWeight.Medium
         )
-        Text(
-            text = "/",
-            fontSize = MaterialTheme.typography.titleLarge.fontSize,
-            fontWeight = FontWeight.Medium
-        )
-        Text(
-            text = "${BrightnessFixedLevel.Max.value}",
-            fontSize = MaterialTheme.typography.titleLarge.fontSize,
-            fontWeight = FontWeight.Medium
-        )
+
+        ProvideTextStyle(textStyle) {
+            Text("$brightnessLevel")
+            Text("/")
+            Text("${BrightnessFixedLevel.Max.value}")
+        }
     }
 }
