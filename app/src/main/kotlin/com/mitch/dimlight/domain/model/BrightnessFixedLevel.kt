58423ea3f0906177c2f37053968f79cd75c11994
Andrea Severi <andrea.severi.dev@gmail.com>
2023-09-20 23:41:49 +0200

    remove param name

--- app/src/main/kotlin/com/mitch/dimlight/domain/model/BrightnessFixedLevel.kt
+++ app/src/main/kotlin/com/mitch/dimlight/domain/model/BrightnessFixedLevel.kt
@@ -1,15 +1,13 @@
 package com.mitch.dimlight.domain.model
 
 sealed class BrightnessFixedLevel(val value: Int) {
-    data object Min : BrightnessFixedLevel(value = FlashlightUtils.brightnessActiveRange.first)
-    data object Half : BrightnessFixedLevel(value = FlashlightUtils.brightnessActiveRange.average().toInt())
-    data object Max : BrightnessFixedLevel(value = FlashlightUtils.brightnessActiveRange.last)
+    data object Min : BrightnessFixedLevel(FlashlightUtils.brightnessActiveRange.first)
+    data object Half : BrightnessFixedLevel(FlashlightUtils.brightnessActiveRange.average().toInt())
+    data object Max : BrightnessFixedLevel(FlashlightUtils.brightnessActiveRange.last)
 }
 
 object FlashlightUtils {
 
     private val brightnessRange = 0..100
     val brightnessActiveRange = brightnessRange.first + 1..brightnessRange.last
-
-    val brightnessDrawableRange = 0..5
 }
