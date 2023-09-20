a598c45ed6b14ab43d063e8e2ae820150be0517c
Andrea Severi <andrea.severi.dev@gmail.com>
2023-09-20 18:15:26 +0200

    add custom layout to center middle chip element in width

--- app/src/main/kotlin/com/mitch/dimlight/ui/screen/home/components/FlashlightBrightnessControls.kt
+++ app/src/main/kotlin/com/mitch/dimlight/ui/screen/home/components/FlashlightBrightnessControls.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.res.stringResource
 import com.mitch.dimlight.R
 import com.mitch.dimlight.domain.model.BrightnessFixedLevel
@@ -25,19 +26,58 @@ fun FlashlightBrightnessControls(
             alignment = Alignment.CenterHorizontally
         )
     ) {
-        AssistChip(
-            onClick = { onControlEmit(BrightnessFixedLevel.Min.value) },
-            label = { Text(stringResource(R.string.min)) }
+        ThreeCenteredChipsLayout(
+            chip1 = {
+                AssistChip(
+                    onClick = { onControlEmit(BrightnessFixedLevel.Min.value) },
+                    label = { Text(stringResource(R.string.min)) }
+                )
+            },
+            chip2 = {
+                AssistChip(
+                    onClick = { onControlEmit(BrightnessFixedLevel.Half.value) },
+                    label = { Text(stringResource(R.string.half)) }
+                )
+            },
+            chip3 = {
+                AssistChip(
+                    onClick = { onControlEmit(BrightnessFixedLevel.Max.value) },
+                    label = { Text(stringResource(R.string.max)) }
+                )
+            }
         )
+    }
+}
 
-        AssistChip(
-            onClick = { onControlEmit(BrightnessFixedLevel.Half.value) },
-            label = { Text(stringResource(R.string.half)) }
-        )
+@Composable
+private fun ThreeCenteredChipsLayout(
+    chip1: @Composable () -> Unit,
+    chip2: @Composable () -> Unit,
+    chip3: @Composable () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Layout(
+        modifier = modifier,
+        content = {
+            chip1()
+            chip2()
+            chip3()
+        }
+    ) { measurables, constraints ->
+        val secondPlaceable = measurables[1].measure(constraints)
+        val otherPlaceables = measurables
+            .filterIndexed { index, _ -> index != 1 }
+            .map { it.measure(constraints) }
 
-        AssistChip(
-            onClick = { onControlEmit(BrightnessFixedLevel.Max.value) },
-            label = { Text(stringResource(R.string.max)) }
-        )
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            secondPlaceable.placeRelative((constraints.maxWidth - secondPlaceable.width) / 2, 0)
+            otherPlaceables.forEachIndexed { index, placeable ->
+                if (index == 0) {
+                    placeable.placeRelative(placeable.width, 0)
+                } else {
+                    placeable.placeRelative((constraints.maxWidth + placeable.width) / 2, 0)
+                }
+            }
+        }
     }
 }
