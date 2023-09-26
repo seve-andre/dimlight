e6b0d58e26ba692537b47496c54c0fec06d563d7
Andrea Severi <andrea.severi.dev@gmail.com>
2023-09-26 15:52:27 +0200

    remove useless derived state

--- app/src/main/kotlin/com/mitch/dimlight/ui/screen/home/HomeScreen.kt
+++ app/src/main/kotlin/com/mitch/dimlight/ui/screen/home/HomeScreen.kt
@@ -8,9 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -49,7 +47,7 @@ fun HomeScreen(
     onTurnOffFlashlight: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val isFlashlightOn by remember { derivedStateOf { brightnessLevel > 0 } }
+    val isFlashlightOn = brightnessLevel > 0
 
     Column(
         modifier = modifier.fillMaxSize(),
