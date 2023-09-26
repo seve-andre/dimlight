9ebae48d4644281d87b9e9f2f6c0543bf24f111a
Andrea Severi <andrea.severi.dev@gmail.com>
2023-09-26 11:02:06 +0200

    remove check for flashlight status from flow

--- app/src/main/kotlin/com/mitch/dimlight/util/flashlight/CameraManagerFlashlightHelper.kt
+++ app/src/main/kotlin/com/mitch/dimlight/util/flashlight/CameraManagerFlashlightHelper.kt
@@ -3,35 +3,20 @@ package com.mitch.dimlight.util.flashlight
 import android.hardware.camera2.CameraAccessException
 import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraManager
-import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
 import javax.inject.Inject
 
 class CameraManagerFlashlightHelper @Inject constructor(
     private val cameraManager: CameraManager
 ) : FlashlightHelper {
+
     private val cameraId = cameraManager.cameraIdList[0]
         ?: throw CameraAccessException(CameraAccessException.CAMERA_ERROR)
+
     private val cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId)
 
-    override val isOn: Flow<Boolean> = callbackFlow {
-        val callback = object : CameraManager.TorchCallback() {
-            override fun onTorchModeChanged(cameraId: String, enabled: Boolean) {
-                trySend(enabled)
-            }
-        }
-
-        cameraManager.registerTorchCallback(callback, null)
-        trySend(cameraManager.getTorchStrengthLevel(cameraId) > 0)
-        awaitClose { cameraManager.unregisterTorchCallback(callback) }
-    }
-
-    override val maxLevel: Int
-        get() {
-            return cameraCharacteristics[CameraCharacteristics.FLASH_INFO_STRENGTH_MAXIMUM_LEVEL]
-                ?: throw CameraAccessException(CameraAccessException.CAMERA_ERROR)
-        }
+    override val maxLevel: Int =
+        cameraCharacteristics[CameraCharacteristics.FLASH_INFO_STRENGTH_MAXIMUM_LEVEL]
+            ?: throw CameraAccessException(CameraAccessException.CAMERA_ERROR)
 
     override fun turnOn(level: Int) {
         require(level in 1..maxLevel)
--- app/src/main/kotlin/com/mitch/dimlight/util/flashlight/FlashlightHelper.kt
+++ app/src/main/kotlin/com/mitch/dimlight/util/flashlight/FlashlightHelper.kt
@@ -1,9 +1,6 @@
 package com.mitch.dimlight.util.flashlight
 
-import kotlinx.coroutines.flow.Flow
-
 interface FlashlightHelper {
-    val isOn: Flow<Boolean>
     val maxLevel: Int
     fun turnOn(level: Int)
     fun turnOff()
