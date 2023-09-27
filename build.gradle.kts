b3cfaf39fa1e738ef045c804105c8a3dec966550
Andrea Severi <andrea.severi.dev@gmail.com>
2023-09-27 12:31:10 +0200

    move kapt to the end of plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.hilt) apply false
-    alias(libs.plugins.kapt) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.detekt) apply false
@@ -11,6 +10,7 @@ plugins {
 
     alias(libs.plugins.gradle.versions)
     alias(libs.plugins.version.catalog.update)
+    alias(libs.plugins.kapt) apply false
 }
 
 tasks.register("clean", Delete::class) {
